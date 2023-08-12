c751c1de4c0cb7bf182099fb328b7d99267b4d65
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-08-12 18:54:14 +0200

    The Implementation for the Get Pin in ASM

--- Code/Secure_the_Bank/get_pin.asm
+++ Code/Secure_the_Bank/get_pin.asm
@@ -1,10 +1,13 @@
 ; This makes your function available to other files
-; This makes your function available to other files
 global get_pin
 
 section .data
 ; ==========================
 ; Your data goes here
+  pin_prompt db "Enter 4-digit PIN: ", 0
+  input db 0
+  output db 0
+  newline db 10
 ; ==========================
 
 section .text
@@ -19,13 +22,37 @@ get_pin:
 ; Your code goes here
 ; 
 ; Print the message that asks for a pin
+  mov rax, 1
+  mov rdi, 1
+  lea rsi, [pin_prompt]
+  mov rdx, 19
+  syscall
 ; 
 ; Read the pin from stdin and store it in a buffer
+  mov rax, 0
+  mov rdi, 0
+  lea rsi, [input]
+  mov rdx, 5
+  syscall
 ; 
 ; Convert the pin to an integer
+  movzx eax, byte [input]
+  sub al, '0'
+  movzx ebx, byte [input + 1]
+  sub bl, '0'
+  movzx ecx, byte [input + 2]
+  sub cl, '0'
+  movzx edx, byte [input + 3]
+  sub dl, '0'
 ; 
 ; Store the integer in eax
-  mov eax, 0 ; This can be deleted, it just keeps function from causing a runtime error until completed
+  mov edi, 10
+  imul eax, edi
+  add eax, ebx
+  imul eax, edi
+  add eax, ecx
+  imul eax, edi
+  add eax, edx
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
   leave
