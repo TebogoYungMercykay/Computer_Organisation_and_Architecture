fdfc671a1547946b97d7ef612f1ddef56d9a7ed8
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-08-13 11:22:59 +0200

    Obscuring the Bank PIN - Final Implementation.

--- Code/Secure_the_Bank/obscure_pin.asm
+++ Code/Secure_the_Bank/obscure_pin.asm
@@ -3,6 +3,7 @@ global obscure_pin
 section .data
 ; ==========================
 ; Your data goes here
+  mask db 0Fh, 0Fh, 0Fh, 0Fh
 ; ==========================
 
 ; void obscure_pin(char* pin)
@@ -15,9 +16,25 @@ obscure_pin:
 ; Do not modify anything above this line unless you know what you are doing
 ; ==========================
 ; Your code goes here
-  ; load PIN digits
-  ; obscure digits
-  ; convert back to ASCII and store in reverse order
+  ; ***** load PIN digits *****
+  mov rsi, rdi  ; rsi holds the address of the string pointer
+  ; ***** obscure digits *****
+  movdqa xmm0, [mask]
+  ; Appliying XOR to the PIN digits with the mask
+  movdqu xmm1, [rsi]
+  pxor xmm1, xmm0
+  movdqu [rsi], xmm1
+  ; ***** convert back to ASCII and store in reverse order *****
+  ; Reversing the First and the Last Digit
+  mov al, byte [rsi+1]
+  mov ah, byte [rsi+2]
+  mov [rsi+1], ah
+  mov [rsi+2], al
+  ; Reversing the Middle Two digits
+  mov al, byte [rsi]
+  mov ah, byte [rsi+3]
+  mov [rsi], ah
+  mov [rsi+3], al
 ; ==========================
 ; Do not modify anything below this line unless you know what you are doing
 
